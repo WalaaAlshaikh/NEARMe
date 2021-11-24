52273b76315fd4e6fc1f20aa02c29537b399fc4f
Walaa Alshaikh <walaalshaikh94@gmail.com>
2021-11-24 11:39:06 +0300

    packages

--- app/src/main/java/com/example/nearmeproject/views/MainActivity.kt
+++ app/src/main/java/com/example/nearmeproject/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.nearmeproject
+package com.example.nearmeproject.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nearmeproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
