233d29ea56f28aa401eebc0d816db3bf9512bb40
ibtihala817 <Ibtihal.ibrahim.dmm.k1@tuwaiq.edu.sa>
2021-11-24 11:15:12 +0300

    comiit

--- app/src/main/java/com/example/nearmeproject/MainActivity.kt
+++ app/src/main/java/com/example/nearmeproject/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//       00000000000000000000000000
-//        setContentView(R.layout.activity_main)
+//       0000000000000000000000000000000
+       setContentView(R.layout.activity_main)
     }
 }
